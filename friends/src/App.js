06775954af33ebbfcfbd553a34fa41dda00457f4
t-tullis <t.tullis34@gmail.com>
2019-02-28 15:33:51 -0800

    completed post and delete requestes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 import FriendsList from './components/FriendsList';
+import NewFriendForm from './components/NewFriendForm';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
+      activeFriend: null,
       friends: []
     }
   }
 
+  //mounts data
   componentDidMount(){
     axios
     .get("http://localhost:5000/friends")
@@ -18,16 +22,51 @@ class App extends Component {
     .catch(err => console.log(err))
   }
 
+  //removes friend
+  removeFriend = (e, id) =>{
+    e.preventDefault();
+    
+    axios
+    .delete(`http://localhost:5000/friends/${id}`)
+    .then(res => this.setState({
+      friends: res.data,
+    }))
+    .catch(err => console.log(err))
+}
+
+
+
+
+
   render() {
     console.log(this.state.friends)
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-           React app is setup
-          </p>
-          <FriendsList friends={this.state.friends} />
+        <header className="navigation">
+          <nav>
+            <div>
+                <Link to='/newfriend'>Add Friend</Link>
+            </div>
+            <div>
+                <Link to ='/'> Home </Link>
+            </div>
+          </nav>
         </header>
+          
+          {/* //Routes  */}
+          <Route 
+          exact 
+          path='/' 
+          render={props => <FriendsList {...props}
+          updateFriend={this.updateFriend}
+          removeFriend={this.removeFriend} 
+          friends={this.state.friends} /> } 
+          />
+          
+          <Route path='/newfriend' render={props => <NewFriendForm  {...props}
+          />} 
+          />
+
       </div>
     );
   }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../App.css'
 
 const FriendsList = props => {
         return(
             <div className='friends-list'>
                 {props.friends.map(friend => {
                     return(
-                    <div key={friend.id}>
-                        <span>{friend.name} </span>
-                        <span>{friend.age} </span>
-                        <span>{friend.email}</span>
+                    <div className='friend-content' key={friend.id}>
+                        <span><strong>Name: </strong>{friend.name} </span>
+                        <span><strong>Age: </strong>{friend.age} </span>
+                        <span><strong>Email: </strong>{friend.email}</span>
+                        <button onClick={e => props.removeFriend(e, friend.id)}>remove friend</button>
+                        <button onClick={props.updateFriend}>Edit</button>
                     </div>
                     )
                 })}
